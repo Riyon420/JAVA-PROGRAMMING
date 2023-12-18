e0483a2292358c0953a1a074c014e01a543f76f2
Riyon420 <riyonmaity420@gmail.com>
2023-12-18 18:52:04 +0530

    recursion

---
+++ recursion.java
@@ -0,0 +1,22 @@
+package JAVA-PROGRAMMING;
+
+public class recursion {
+    class recursion {
+        int result = 1;
+
+        int cal(int a) {
+            if (a == 1) {
+                return 1;
+            } else {
+                return a * cal(a - 1);
+            }
+        }
+
+        public static void main(String[] args) {
+            recursion r = new recursion();
+            int s = r.cal(5);
+            System.out.println(s);
+        }
+
+    }
+}
